ea4ba84624ff9e18e26ac0678e727de71e059b9b
Jona Ekenberg <jona@dsv.su.se>
2014-10-08 13:32:03 +0200

    Basic node class added.

--- js/node.js
+++ js/node.js
@@ -1,8 +1,23 @@
 var sense4us = sense4us || {};
 
-sense4us.node = function() {
-	var that = {
+sense4us.node = function(id) {
+	var html_entity = document.getElementById(id);
 
+	if (!html_entity) {
+		html_entity = document.createElement("div");
+		html_entity.setAttribute("id", id);
+		document.body.appendChild(html_entity);
+
+		console.log(html_entity);
+	}
+
+	var that = {
+		set: function(name, value) {
+			html_entity.setAttribute("data-" + name, value);
+		},
+		get: function(name) {
+			return html_entity.getAttribute("data-" + name);
+		}
 	}
 
 	return that;
