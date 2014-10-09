ffc962f2cdeb4847b63f1c1d751dec3fe3c30cf4
Anton Haughey <anton@egovlab.eu>
2014-10-09 15:16:29 +0200

    The inspector-panel now updates the html elements upon changes

--- js/inspector.js
+++ js/inspector.js
@@ -14,43 +14,55 @@ var sense4us = sense4us || {};
 sense4us.inspector = function() {
 	var inspectingObject = null;
 
+	/**
+    * Generates and returns the HTML content of the inspector panel.
+    * The HTML content varies depending upon what object is being inspected.
+    * Any changes made using the inspector-panel will trigger an "object_updated" event.
+    * @method generateHTML
+    * @private
+    * @returns {String} The html content of the inspector panel
+    */
+	var generateHTML = function() {
+		if (!inspectingObject) return null;
+		html = "";
+		html += "<form id='" + inspectingObject.id + "-form'>";
+		for (var property_name in inspectingObject) {
+			var property = inspectingObject[property_name];
+			// This if-statement will filter out all object properties which are functions and objects.
+			if (!(property instanceof Function) && !(property instanceof Object)) {
+				html += "<p>" + property_name + ": ";
+				html += "<input type='text' name='"+property_name+"' value='"+property+"'></input>";
+			}
+		}
+		html += "</form>";
+
+		return html;
+	}
+
 	var that = {
 		/**
-	    * Selects an object to inspect using the inspector-panel.
+	    * Selects an object to inspect and refreshes the inspector-panel.
 	    * @method inspect
 	    * @param object {Object} This parameter defines the object we want to inspect.
 	    */
 		inspect: function(object) {
-			sense4us.inspector.inspectingObject = object;
-			console.log("inspecting object: " + sense4us.inspector.inspectingObject);
-			sense4us.inspector.generateHTML();
-		},
-
-		/**
-	    * Generates HTML for the inspector panel showing the attributes/properties of
-	    * the currently inspectedObject.
-	    * @method generateHTML
-	    */
-		generateHTML: function() {
-			html = "";
-			for (var property_name in sense4us.inspector.inspectingObject) {
-				var property = sense4us.inspector.inspectingObject[property_name];
-				// This if-statement will filter out all object properties which are functions and objects.
-				if (!(property instanceof Function) && !(property instanceof Object)) {
-					html += "<p>" + property_name + ": ";
-					html += "<input type='text' value='"+property+"'></input>";
-				}
-			}
+			inspectingObject = object;
+			var html = generateHTML();
 			$( "div.inspector" ).html(html);
-			return html;
-		}
+			if (html) {
+				$("#" + inspectingObject.id + "-form").find("input").change(function(event) {
+					inspectingObject.set($(this).prop("name"), $(this).val());
+					sense4us.events.trigger("object_updated", inspectingObject);
+				});
+			}
+		},
 	}
 
 	return that;
 }();
 
 /**
-* Event that fires when an object have been selected.
+* Triggers when an object have been selected.
 * This triggers the inspector-panel change focus onto the
 * newly selected object and its properties.
 * @event object_selected
@@ -61,7 +73,7 @@ sense4us.events.bind("object_selected", function(object) {
 });
 
 /**
-* Event that fires when an object have been deselected.
+* Triggers when an object have been deselected.
 * This triggers the inspector-panel remove focus of the previously selected object.
 * @event object_deselected
 * @param {Object} object The deselected object
