3ecaf8389958181ab10c8d4c374102db6d816d2e
Anton Haughey <anton@egovlab.eu>
2014-10-13 11:49:42 +0200

    Nothing changed

--- server/network.js
+++ server/network.js
@@ -1,12 +1,12 @@
 module.exports = function(server)
 {
-	var io = require('socket.io')(server);
+	//var io = require('socket.io')(server);
 
 	var that = {
-		
+		io: require('socket.io')(server)
 	};
 
-	io.on('connection', function(socket) {
+	that.io.on('connection', function(socket) {
 	    socket.emit('network_client_connected', { state: 'successful' });
 
 	    socket.on('network_send_data', function (data) {
