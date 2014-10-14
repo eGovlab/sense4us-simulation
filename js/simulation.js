98a1d293307835021294bc7701fd26b0ac312643
Anton Haughey <anton@egovlab.eu>
2014-10-14 16:29:09 +0200

    Simulation is now using the actual nodes creates and linked using the gui. A button is also added to run the simulation

--- js/simulation.js
+++ js/simulation.js
@@ -3,20 +3,29 @@ var sense4us = sense4us || {};
 
 sense4us.simulation = function() {
 	var that = {
-		run: function(nodes, links) {
-			sense4us.network.sendData("run_simulation", 
-				[
-					[
-						{"id":"newnode-0", "sig": 0, "fire": 1},
-						{"id":"newnode-1", "sig": 0, "fire": 0},
-						{"id":"newnode-2", "sig": 0, "fire": 0}
-					],
-					[
-						{"id": "newline-3", "n1":"newnode-0", "n2":"newnode-1", "co": 0.5, "t": 1},
-						{"id": "newline-4", "n1":"newnode-1", "n2":"newnode-2", "co": 0.5, "t": 2}
-					]
-				]
-			);
+		run: function() {
+			var nodes = [];
+			var links = [];
+
+			var entities = sense4us.entities.id_to_entity;
+			for (var entityIndex in entities) {
+				var entity = entities[entityIndex];
+				var className = entity.get_element().getAttribute("class");
+				switch (className) {
+			    case "node":
+					entity.signal = parseFloat(entity.signal);
+					entity.signal_fire = parseFloat(entity.signal_fire);
+			    	nodes.push(entity);
+			        break;
+			    case "link":
+					entity.co = parseFloat(entity.co);
+					entity.t = parseFloat(entity.t);
+			    	links.push(entity);
+			        break;
+				}
+			}
+
+			sense4us.network.sendData("run_simulation", [nodes, links]);
 		}
 	};
 
@@ -24,13 +33,11 @@ sense4us.simulation = function() {
 }();
 
 sense4us.network.socket.on("run_simulation_completed", function(nodes) {
-	console.log("The simluation have been completed", nodes);
-
 	for (var nodeIndex in nodes) {
 		var node = nodes[nodeIndex];
 		var node_entity = sense4us.entities.id_to_entity[node.id];
 
-		node_entity.set("value", node["sig"]);
+		node_entity.set("signal", node["signal"]);
 		node_entity.events.trigger("update", node_entity);
 	}
 });
