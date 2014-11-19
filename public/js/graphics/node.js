2e5bba6d3b26d72fa7f3938f9a7bb72a42fa9dfc
Robin Swenson <rnswenson@gmail.com>
2014-11-19 20:35:44 +0100

    Replaced node labels to just display signal.

--- public/js/graphics/node.js
+++ public/js/graphics/node.js
@@ -21,18 +21,18 @@ sense4us.graphics.node = function(entity, stage) {
 		[0, 1], 0, 0, 50, 
 		-8, -8, 46).drawCircle(0, 0, color.get_property("circle_radius"));
 
-	var signal_label = new createjs.Text(entity.signal, "bold 14px Arial", color.get_color("label"));
-	signal_label.textAlign = "right";
+	var signal_label = new createjs.Text("THIS NOT BE USED", "bold 14px Arial", color.get_color("label"));
+	signal_label.textAlign = "center";
 	signal_label.y = -7;
-	signal_label.x = -3;
+	//signal_label.x = -3;
 	signal_label.shadow = new createjs.Shadow(color.get_color("label_shadow"), 0, 0, color.get_property("label_shadow_blur"));
 
-	var signal_fire_label = new createjs.Text("(" + entity.signal_fire + ")", "bold 14px Arial", color.get_color("label"));
+	/*var signal_fire_label = new createjs.Text("(" + entity.signal_fire + ")", "bold 14px Arial", color.get_color("label"));
 	signal_fire_label.textAlign = "left";
 	signal_fire_label.x = 3;
 	signal_fire_label.y = -7;
 	signal_fire_label.shadow = new createjs.Shadow(color.get_color("label_shadow"), 0, 0, color.get_property("label_shadow_blur"));
-
+	*/
 	var name_label = new createjs.Text(entity.id, "bold 14px Arial", color.get_color("label"));
 	name_label.textAlign = "center";
 	name_label.y = -75;
@@ -40,7 +40,8 @@ sense4us.graphics.node = function(entity, stage) {
 
 	var circle_container = new createjs.Container();
 
-	circle_container.addChild(border_circle, circle, signal_label, signal_fire_label, name_label);
+	//circle_container.addChild(border_circle, circle, signal_label, signal_fire_label, name_label);
+	circle_container.addChild(border_circle, circle, signal_label, name_label);
 
 	var that = Object.create(sense4us.graphics.graphic(entity, stage));
 
@@ -50,8 +51,9 @@ sense4us.graphics.node = function(entity, stage) {
 	that.container.x = that.container.y = 100;
 
 	that.update = function() {
-		signal_label.text = entity.signal;
-		signal_fire_label.text = entity.signal_fire;
+		//entity.signal
+		signal_label.text = (parseFloat(entity.signal)*100).toFixed(2) + "%";
+		//signal_fire_label.text = entity.signal_fire;
 		name_label.text = entity.name;
 		that.container.x = parseInt(entity.x);
 		that.container.y = parseInt(entity.y);
