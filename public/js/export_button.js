c049445e056580bb1d0ee0c01c7b07f873df2b79
Jona <demonflip2@gmail.com>
2014-11-19 15:09:08 +0100

    changed look of save button and renamed it into export button

--- public/js/export_button.js
+++ public/js/export_button.js
@@ -1,12 +1,12 @@
 "use strict";
 
 (function(namespace) {
-	$("<a href='#' download='data.json' id='save_button' class='button'>Export (.json)</a>").prependTo("#menu");
+	$("<a href='#' download='data.json' id='export_button' class='button'>Export (.json)</a>").prependTo("#menu");
 
-	$("#save_button").click(function() {
+	$("#export_button").click(function() {
 		var obj = namespace.simulation.get_nodes_and_links();
 		var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
 		
-	    $("#save_button").prop("href", "data:" + data);
+	    $("#export_button").prop("href", "data:" + data);
 	});
 }(window.sense4us = window.sense4us || {}));
