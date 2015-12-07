b4d417eb0d918ad660d3616872334255a6c8e246
Robin <rnswenson@gmail.com>
2015-12-07 14:38:25 +0100

    Now the text wont hack about when loading new nodes for the first time.

--- frontend/public/js/graphics/draw_text.js
+++ frontend/public/js/graphics/draw_text.js
@@ -8,13 +8,13 @@ module.exports = function drawText(ctx, text, x, y, color, centered, size, font,
         fontColor    = color    || 'rgba(80,80,80,1)',
         fontBaseline = baseline || 'top';
 
-    if(centered) {
-        textX = textX - (ctx.measureText(text).width / 2);
-    }
-
     ctx.textBaseline = fontBaseline;
     ctx.fillStyle    = fontColor;
     ctx.font         = fontSize + "px " + fontType;
+
+    if(centered) {
+        textX = textX - (ctx.measureText(text).width / 2);
+    }
 
     ctx.fillText(text, textX, textY);
 };
