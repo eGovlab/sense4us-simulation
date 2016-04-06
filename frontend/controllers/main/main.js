9a96d36081dfedc29af33d6b736389e0418027e7
Robin <rnswenson@gmail.com>
2016-04-06 17:07:02 +0200

    Added server info to config.

--- frontend/controllers/main/main.js
+++ frontend/controllers/main/main.js
@@ -1,7 +1,18 @@
 'use strict';
 
+var CONFIG = require("rh_config-parser");
+
+var protocol = CONFIG.get("SENSE4US", "tool", "protocol"),
+    hostname = CONFIG.get("SENSE4US", "tool", "hostname"),
+    port     = CONFIG.get("SENSE4US", "tool", "port");
+
 function root(req, res, next) {
-    res.render("main");
+
+    res.render("main", {locals: {
+        protocol: protocol,
+        hostname: hostname,
+        port:     port
+    }});
 }
 
 function wizard(req, res, next) {
